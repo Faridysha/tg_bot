c132515777458f3577879e1495a0ef3dbed2e8c1
Faridysha <roflopochta@mail.ru>
2023-05-09 10:44:33 +0300

    add await for async

--- index.js
+++ index.js
@@ -11,20 +11,20 @@ bot.start((ctx) => ctx.reply('Бот работает, ура!'));
 //bot.on(message('sticker'), (ctx) => ctx.reply('Классный стикер!'));
 //bot.hears('Лох', (ctx) => ctx.reply('Сам лох!'))
 bot.hears('Гифка', async (ctx) => {
-    ctx.replyWithAnimation(Input.fromURLStream('https://cataas.com/cat/gif'));
+   await ctx.replyWithAnimation(Input.fromURLStream('https://cataas.com/cat/gif'));
    console.log(ctx.message.from.username)
    console.log(ctx.message.text)
    
 });
 bot.hears('Рандом', async (ctx) => {
-    ctx.replyWithPhoto(Input.fromURLStream('https://cataas.com/cat'));
+    await ctx.replyWithPhoto(Input.fromURLStream('https://cataas.com/cat'));
    console.log(ctx.message.from.username)
    console.log(ctx.message.text)
    
 });
 bot.hears(message, async (ctx) => {
 
-    ctx.replyWithPhoto(Input.fromURLStream('https://cataas.com/cat/says/' + ctx.message.text));
+    await ctx.replyWithPhoto(Input.fromURLStream('https://cataas.com/cat/says/' + ctx.message.text));
    console.log(ctx.message.from.username)
    console.log(ctx.message.text)
    
